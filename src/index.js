1f560522fdf8923eb6f5b49d47be0c89d9c3f10c
yangyangran <ranyangyangme@gmail.com>
2018-11-24 10:15:14 +0800

    feat:add build

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Flex from "./Flex";
 
-ReactDOM.render(<Flex />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
